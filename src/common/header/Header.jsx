c0e8cf979bb8e10badccc80a1ed0d19081d2e7de
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-24 20:48:30 -0300

    sexto commit

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import Head from "./Head";
-import Navbar from "./Navbar"
+import React from "react"
+import "./Header.css"
+import Head from "./Head"
 import Search from "./Search"
+import Navbar from "./Navbar"
 
-const Header = () => {
-    return (
-        <>
-            <Head />
-            <Search/>
-            <Navbar/>
-        </>
-    )
+const Header = ({ CartItem }) => {
+  return (
+    <>
+      <Head/>
+      <Search CartItem={CartItem}/>
+      <Navbar/>
+    </>
+  )
 }
 
 export default Header
