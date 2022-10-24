5e7ac36b3856954bba410668c8e65be318be2112
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-23 22:21:08 -0300

    quinto commit

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -3,24 +3,24 @@ import React from "react";
 const Head = () => {
     return (
         <>
-        <section className='head'>
-          <div className='container d_flex'>
-            <div className='left row'>
-              <i className='fa fa-phone'></i>
-              <label> +88012 3456 7894</label>
-              <i className='fa fa-envelope'></i>
-              <label> support@ui-lib.com</label>
-            </div>
-            <div className='right row RText'>
-              <label>Theme FAQ"s</label>
-              <label>Precisa de ajuda?</label>
-              <span>🏳️‍⚧️</span>
-              <label>BR</label>
-              <span>🏳️‍⚧️</span>
-              <label>USD</label>
-            </div>
-          </div>
-        </section>
+            <section className='head'>
+                <div className='container d_flex'>
+                    <div className='left row'>
+                    <i className='fa fa-phone'></i>
+                    <label> +88012 3456 7894</label>
+                    <i className='fa fa-envelope'></i>
+                    <label> support@ui-lib.com</label>
+                    </div>
+                    <div className='right row RText'>
+                    <label>Theme FAQ"s</label>
+                    <label>Precisa de ajuda?</label>
+                    <span>🏳️‍⚧️</span>
+                    <label>BR</label>
+                    <span>🏳️‍⚧️</span>
+                    <label>USD</label>
+                    </div>
+                </div>
+            </section>
       </>
     )
 }
---
+++ src/components/shops/Sdata.js
@@ -0,0 +1,68 @@
+const Sdata = {
+    shopItems: [
+      {
+        id: 7,
+        cover: "./images/shops/cronogramacapilar.png",
+        name: "Mapple Earphones",
+        price: "180",
+        discount: "25",
+      },
+      {
+        id: 8,
+        cover: "./images/shops/kitcremedepentear.png",
+        name: "Vivo android one",
+        price: "120",
+        discount: "10",
+      },
+      {
+        id: 9,
+        cover: "./images/shops/kitdefinicao.png",
+        name: "Sony Light",
+        price: "20",
+        discount: "50 ",
+      },
+      {
+        id: 10,
+        cover: "./images/shops/kitsha.png",
+        name: "Iphone",
+        price: "999",
+        discount: "10 ",
+      },
+      {
+        id: 11,
+        cover: "./images/shops/kitsos.png",
+        name: "Ceats wireless earphone",
+        price: "80",
+        discount: "20 ",
+      },
+      {
+        id: 12,
+        cover: "./images/shops/kitsos.png",
+        name: "Redimi Phone",
+        price: "400",
+        discount: "20 ",
+      },
+      {
+        id: 13,
+        cover: "./images/shops/kittonico.png",
+        name: "Xeats Bluetooth earphones",
+        price: "60",
+        discount: "5 ",
+      },
+      {
+        id: 14,
+        cover: "./images/shops/kittonico.png",
+        name: "Airpod",
+        price: "120",
+        discount: "10",
+      },
+      {
+        id: 15,
+        cover: "./images/shops/kitdefinicao.png",
+        name: "Silver Cap",
+        price: "5",
+        discount: "2",
+      },
+    ],
+  }
+  export default Sdata
