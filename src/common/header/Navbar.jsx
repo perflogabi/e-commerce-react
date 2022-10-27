c8b54b8be7db0393a184bbdd59211ff474b72091
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-26 21:53:19 -0300

    quinto commit

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
+
+    const [MobileMenu, setMobileMenu] = useState(false)
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+       <>
+        <header>
+            <div className="container d_flex">
+                <div className="catgrories d_flex">
+                    <span className="fa-solid fa-border-all"></span>
+                    <h4>
+                        Categorias <i className="fa fa-cheverson down"></i>
+                    </h4>
+                </div>
+                
+            </div>
+        </header>
+       </>
     )
 }
 
