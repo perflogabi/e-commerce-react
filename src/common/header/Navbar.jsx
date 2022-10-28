3806d0ed8a162eef545569b62b998193c9d4cc9f
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-27 21:39:14 -0300

    quinto commit

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -14,7 +14,33 @@ const Navbar = () => {
                         Categorias <i className="fa fa-cheverson down"></i>
                     </h4>
                 </div>
-                
+                <div className='navlink'>
+                    <ul className={MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"} onClick={() => setMobileMenu(false)}>
+                        {/*<ul className='link f_flex uppercase {MobileMenu ? "nav-links-MobileMenu" : "nav-links"} onClick={() => setMobileMenu(false)}'>*/}
+                        <li>
+                            <Link to='/'>home</Link>
+                        </li>
+                        <li>
+                            <Link to='/pages'>pages</Link>
+                        </li>
+                        <li>
+                            <Link to='/user'>user account</Link>
+                        </li>
+                        <li>
+                            <Link to='/vendor'>vendor account</Link>
+                        </li>
+                        <li>
+                            <Link to='/track'>track my order</Link>
+                        </li>
+                        <li>
+                            <Link to='/contact'>contact</Link>
+                        </li>
+                    </ul>
+
+                    <button className='toggle' onClick={() => setMobileMenu(!MobileMenu)}>
+                        {MobileMenu ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
+                    </button>
+                </div>
             </div>
         </header>
        </>
