1138258cf56448b20ec6f6976dfe77fed1c2cf46
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-31 21:52:26 -0300

    nono commit

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-import logo from "./components/assets/images/logo.svg"
+import { Link } from "react-router-dom";
+import logo from "../../components/assets/images/logo.svg"
 
 const Search = () => {
     return (
         <>
-            <section>
+            <section className="search">
                 <div className="container c_flex">
                     <div className="logo width">
                         <img src={logo} alt="" />
+                    </div>
+                </div>
+
+                <div className="search-box f_flex">
+                    <i className="fa fa-search"></i>
+                    <input type="text" placeholder="Pesquise aqui..." />
+                    <span>Todas Categorias</span>
+                </div>
+
+                <div className="icon f_flex width">
+                    <i className="fa fa-user icon_circle"></i>
+                    <div className="cart">
+                        <Link to='/cart'>
+                            <i className="fa fa-shopping-bag icon-circle"></i>
+                            <span>0</span>
+                        </Link>
                     </div>
                 </div>
             </section>
