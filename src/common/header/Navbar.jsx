02c576863110b856e889cff7ebc4925dcd89dd37
Gabriela Florêncio <perflogabi@gmail.com>
2022-11-01 22:49:23 -0300

    decimo commit

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
        <>
         <header>
             <div className="container d_flex">
-                <div className="catgrories d_flex">
+                <div className="categories d_flex">
                     <span className="fa-solid fa-border-all"></span>
                     <h4>
                         Categorias <i className="fa fa-cheverson down"></i>
