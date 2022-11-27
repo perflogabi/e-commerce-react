617ecd547b338b001e495f49ab2732aea0bc886d
Gabriela Florêncio <perflogabi@gmail.com>
2022-11-27 19:10:56 -0300

    decimo5 commit

--- src/components/MainPage/Sdata.js
+++ src/components/MainPage/Sdata.js
@@ -3,25 +3,25 @@ const Sdata = [
       id: 1,
       title: "50% Off For Your First Shopping",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      cover: "./images/SlideCard/escovas.png",
+      cover: "./images/SlideCard/kitd.png",
     },
     {
       id: 2,
       title: "50% Off For Your First Shopping",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      cover: "./images/SlideCard/escovas.png",
+      cover: "./images/SlideCard/kitd.png",
     },
     {
       id: 3,
       title: "50% Off For Your First Shopping",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      cover: "./images/SlideCard/escovas.png",
+      cover: "./images/SlideCard/kitd.png",
     },
     {
       id: 4,
       title: "50% Off For Your First Shopping",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis lobortis consequat eu, quam etiam at quis ut convallis.",
-      cover: "./images/SlideCard/escovas.png",
+      cover: "./images/SlideCard/kitd.png",
     },
   ]
   export default Sdata
