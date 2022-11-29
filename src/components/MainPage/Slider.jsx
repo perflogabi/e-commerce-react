3ef59f9b97f498916b09224115cfbc2217b95219
Gabriela Florêncio <perflogabi@gmail.com>
2022-11-28 23:26:40 -0300

    decimo6 commit

--- src/components/MainPage/Slider.jsx
+++ src/components/MainPage/Slider.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { SlideCard } from './SlideCard'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export const Slider = () => {
   return (
     <div>
         <section className='homeSlide contentWidth'>
             <div className="container">
-                  <SlideCard /> 
-                
+                  <SlideCard />    
             </div>
         </section>
     </div>
